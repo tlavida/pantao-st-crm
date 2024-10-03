f6d6a364935a1b57a7c7fde94538c52f50286b52
tlavida <93471499+tlavida@users.noreply.github.com>
2024-10-03 17:12:23 +0300

    Add packages

---
+++ pantao/src/main/java/pantao_st_crm/com/pantao/PantaoApplication.java
@@ -0,0 +1,13 @@
+package pantao_st_crm.com.pantao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PantaoApplication.class, args);
+	}
+
+}
---
+++ pantao/src/test/java/pantao_st_crm/com/pantao/PantaoApplicationTests.java
@@ -0,0 +1,13 @@
+package pantao_st_crm.com.pantao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

23badfc3adfe3fea029eda7ea65d83f4abba1716
tlavida <93471499+tlavida@users.noreply.github.com>
2024-10-28 21:07:34 +0300

    Добавила секьюрити и таблицу покупателей

---
+++ src/main/java/com/pantao_st_crm/config/Sha256PasswordEncoder.java
@@ -0,0 +1,26 @@
+package com.pantao_st_crm.config;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class Sha256PasswordEncoder implements PasswordEncoder {
+    @Override
+    public String encode(CharSequence rawPassword) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] digest = md.digest(rawPassword.toString().getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(digest);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return encode(rawPassword).equals(encodedPassword);
+    }
+}
---
+++ src/main/java/com/pantao_st_crm/entity/Customer.java
@@ -0,0 +1,30 @@
+package com.pantao_st_crm.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customer")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "phone", nullable = false)
+    private String phone;
+    @Column(name = "address", nullable = false)
+    private String address;
+}
